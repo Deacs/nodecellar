869173a29c1de57408692c58d1723fc50b0d1e55
System Administrator <root@Davids-MacBook-Pro.local>
2013-08-21 22:54:11 +0100

    MongoDB included

--- server.js
+++ server.js
@@ -2,18 +2,21 @@ var express = require('express'),
 	wines = require('./routes/wines');
  
 var app = express();
- 
+
+app.configure(function() {
+	app.use(express.logger('dev')); /* 'default', 'short', 'tiny', 'dev' */
+	app.use(express.bodyParser());
+});
+
 app.get('/', function (req, res) {
 	res.send('Nodecellar Application');
 });
+
 app.get('/wines/', wines.findAll);
 app.get('/wines/:id', wines.findById);
-// app.get('/wines', function(req, res) {
-//     res.send([{name:'wine1'}, {name:'wine2'}]);
-// });
-// app.get('/wines/:id', function(req, res) {
-//     res.send({id:req.params.id, name: "The Name", description: "description"});
-// });
- 
+app.post('/wines', wines.addWine);
+app.put('/wines/:id', wines.updateWine);
+// app.delete('/wines/:id', wines.deleteWine);
+
 app.listen(3000);
 console.log('Listening on port 3000...');
