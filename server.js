f6969c4c034ae21f80b201fa299bd2ffe177bf4d
System Administrator <root@Davids-MacBook-Pro.local>
2013-08-21 13:26:22 +0100

    Pre MongoDB hook up

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-var express = require('express');
+var express = require('express'),
+	wines = require('./routes/wines');
  
 var app = express();
  
-app.get('/wines', function(req, res) {
-    res.send([{name:'wine1'}, {name:'wine2'}]);
+app.get('/', function (req, res) {
+	res.send('Nodecellar Application');
 });
-app.get('/wines/:id', function(req, res) {
-    res.send({id:req.params.id, name: "The Name", description: "description"});
-});
+app.get('/wines/', wines.findAll);
+app.get('/wines/:id', wines.findById);
+// app.get('/wines', function(req, res) {
+//     res.send([{name:'wine1'}, {name:'wine2'}]);
+// });
+// app.get('/wines/:id', function(req, res) {
+//     res.send({id:req.params.id, name: "The Name", description: "description"});
+// });
  
 app.listen(3000);
 console.log('Listening on port 3000...');
